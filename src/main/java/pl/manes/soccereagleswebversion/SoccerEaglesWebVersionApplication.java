a98cc0ba9b86aef155b0c896a960b145a9635fe1
Manes79 <mszornel@gmail.com>
2022-10-02 19:25:39 +0200

    first commit

---
+++ src/main/java/pl/manes/soccereagleswebversion/SoccerEaglesWebVersionApplication.java
@@ -0,0 +1,13 @@
+package pl.manes.soccereagleswebversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerEaglesWebVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerEaglesWebVersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/manes/soccereagleswebversion/SoccerEaglesWebVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manes.soccereagleswebversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerEaglesWebVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
